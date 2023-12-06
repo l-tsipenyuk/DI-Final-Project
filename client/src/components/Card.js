2e98391447d95b0ab186d4cda78f3351c47f6c05
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-12-07 00:43:05 +0200

    check gif preview in card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
-
 const BASE_URL = process.env.REACT_APP_BASE_URL
 const GIPHY_KEY = process.env.REACT_APP_SEARCH_IMAGE_GIPHY_API_KEY
 
@@ -13,7 +12,8 @@ const Card = (props) => {
 
     const [imagePaste, setImagePaste] = useState(false);
     const [imageSearch, setImageSearch] = useState(false);
-    const [giphyImage, setGiphyImage] = useState('');
+
+    const [previewImage, setPreviewImage] = useState('');
 
     const param = useParams();
     const navigate = useNavigate();
@@ -29,7 +29,7 @@ const Card = (props) => {
             if (!res.ok) {
                 console.error(`Error with data fetching. Status: ${res.status}`);
                 setCard([]);
-                setImage('image1.png');
+                // setImage('image1.png');
                 setName('');
                 return;
             }
@@ -41,7 +41,7 @@ const Card = (props) => {
         catch (e) {
             console.log('An error occured:', e);
             setCard([]);
-            setImage('image1.png');
+            // setImage('image1.png');
             setName('');
         }
     };
@@ -69,8 +69,8 @@ const Card = (props) => {
 
     const exitEditMode = () => {
         setEditMode(false);
-        setImage('');
-        setName('');
+        // setImage('');
+        // setName('');
     };
 
     // -----methods to handle image search/image URL paste-----
@@ -88,21 +88,23 @@ const Card = (props) => {
         setImageSearch(true);
     };
 
-    const exitImageSearch = () => {
+    const exitImageSearch = async () => {
         setImageSearch(false);
-        setImage('');
+        await setImage('');
     };
 
     // --------------------------------------------------------
 
     const fetchImages = async (searchWord) => {
         try {
-            const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`);
+            const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=${GIPHY_KEY}`);
             const giphyData = await res.json();
             if (giphyData && giphyData.data.length > 0) {
                 const randomIndex = Math.floor(Math.random() * giphyData.data.length);
                 const giphyImageNew = giphyData.data[randomIndex].images.original.url;
                 setImage(giphyImageNew);
+
+                // setPreviewImage(giphyImageNew);
             }
         } catch (e) { console.log(e) }
     }
@@ -128,11 +130,14 @@ const Card = (props) => {
 
             setCard(data);
             exitEditMode();
-            // setImage(data.image);
         } catch (e) {
             console.log(e);
         }
     }
+
+    useEffect(() => {
+        setPreviewImage(image);
+    }, [image])
 
     return (
         <div>
@@ -170,7 +175,7 @@ const Card = (props) => {
                                 ) : (null)
                                 }
 
-                                {imageSearch ? (
+                                {/* {imageSearch ? (
                                     <div>
                                         <form onSubmit={(e) => {
                                             e.preventDefault();
@@ -178,13 +183,37 @@ const Card = (props) => {
                                             exitImageSearch();
                                         }}>
                                             Search GIF: <input onChange={(e) => setImage(e.target.value)} />
+
+                                            <div id="gif-preview"></div>
                                             <button type="submit">Go</button>
+
+
+
+                                        </form>
+                                    </div>
+                                ) : null} */}
+
+                                {imageSearch ? (
+                                    <div>
+                                        <form onSubmit={(e) => {
+                                            e.preventDefault();
+                                            fetchImages(image);
+                                            exitImageSearch();
+                                        }}>
+                                            Search GIF: <input onChange={(e) => setImage(e.target.value)} />
+                                            <button type="submit">Go</button>
+                                            <div id="gif-preview">
+                                                <p>GIF Preview:</p>
+                                                <img src={image} alt="Invalid Image URL" width="90" height="90" /><br />
+                                            </div>
+
+
                                         </form>
                                     </div>
                                 ) : null}
 
                                 {/* Image URL: <input value={image} onChange={(e) => setImage(e.target.value)} /><br /> */}
-                                
+
                                 <form onSubmit={edit}>
                                     Word: <input value={name} onChange={(e) => setName(e.target.value)} /><br />
                                     <input type="submit" value="Save" />
@@ -215,3 +244,233 @@ const Card = (props) => {
 }
 
 export default Card;
+
+
+
+// import React, { useEffect, useState, useRef } from "react";
+// import { useParams, Link, useNavigate } from "react-router-dom";
+
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const GIPHY_KEY = process.env.REACT_APP_SEARCH_IMAGE_GIPHY_API_KEY;
+
+// const Card = (props) => {
+//     const [card, setCard] = useState([]);
+//     const [image, setImage] = useState('');
+//     const [name, setName] = useState('');
+//     const [editMode, setEditMode] = useState(false);
+//     const [previewImage, setPreviewImage] = useState('');
+
+//     const [imagePaste, setImagePaste] = useState(false);
+//     const [imageSearch, setImageSearch] = useState(false);
+
+//     const [randomPreviewImage, setRandomPreviewImage] = useState('');
+
+//     const param = useParams();
+//     const navigate = useNavigate();
+//     const previewRef = useRef();
+
+//     useEffect(() => {
+//         getCardInfo();
+//     }, []);
+
+//     const getCardInfo = async () => {
+//         try {
+//             const res = await fetch(`${BASE_URL}/api/cards/${param.id}`);
+
+//             if (!res.ok) {
+//                 console.error(`Error with data fetching. Status: ${res.status}`);
+//                 setCard([]);
+//                 setName('');
+//                 return;
+//             }
+
+//             const data = await res.json();
+//             setCard(data);
+//             setName(data.name);
+//         } catch (e) {
+//             console.log('An error occurred:', e);
+//             setCard([]);
+//             setName('');
+//         }
+//     };
+
+//     const del = async () => {
+//         try {
+//             await fetch(`${BASE_URL}/api/cards/${param.id}`, {
+//                 method: 'DELETE',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//             });
+
+//             navigate('/study');
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
+
+//     const activateEditMode = () => {
+//         setEditMode(true);
+//     };
+
+//     const exitEditMode = () => {
+//         setEditMode(false);
+//     };
+
+//     const activateImagePaste = () => {
+//         setImagePaste(true);
+//         setImage('');
+//     };
+
+//     const exitImagePaste = () => {
+//         setImagePaste(false);
+//     };
+
+//     const activateImageSearch = () => {
+//         setImageSearch(true);
+//         setImage('');
+//         setPreviewImage('');
+//     };
+
+//     const exitImageSearch = () => {
+//         setImageSearch(false);
+//         setPreviewImage('');
+//     };
+
+//     const fetchImages = async (searchWord) => {
+//         try {
+//             const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=${GIPHY_KEY}`);
+//             const giphyData = await res.json();
+//             if (giphyData && giphyData.data.length > 0) {
+//                 const randomIndex = Math.floor(Math.random() * giphyData.data.length);
+//                 const giphyImageNew = giphyData.data[randomIndex].images.original.url;
+//                 setImage(giphyImageNew);
+//                 setPreviewImage(giphyImageNew);
+//             }
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
+
+//     const fetchRandomImage = async () => {
+//         try {
+//             const res = await fetch(`https://api.giphy.com/v1/gifs/search?ids=${image}&rating=g&api_key=${GIPHY_KEY}`);
+//             const giphyData = await res.json();
+//             if (giphyData && giphyData.data.length > 0) {
+//                 const randomIndex = Math.floor(Math.random() * giphyData.data.length);
+//                 const randomImage = giphyData.data[randomIndex].images.original.url;
+//                 setRandomPreviewImage(randomImage);
+//             }
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
+
+
+//     const edit = async () => {
+//         try {
+//             const body = {}
+//             if (name && name.trim() !== '') {
+//                 body.name = name.trim()
+//             }
+//             if (image && image.trim() !== '') {
+//                 body.image = image.trim()
+//             }
+//             const res = await fetch(`${BASE_URL}/api/cards/${param.id}`, {
+//                 method: 'PUT',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify(body)
+//             });
+//             const data = await res.json();
+
+//             setCard(data);
+//             exitEditMode();
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             {card && card.length > 0 && card.map((item) => {
+//                 return (
+//                     <div key={item.id}
+//                         style={{
+//                             display: "inline-block",
+//                             border: "1px solid black",
+//                             margin: "20px"
+//                         }}>
+//                         <img src={item.image} alt="Invalid Image URL" width="90" height="90" />
+//                         <h4>{item.name}</h4>
+
+//                         {editMode ? (
+//                             <div>
+//                                 <div className="Toggle">
+//                                     <button onClick={activateImageSearch}>Search GIF</button>
+//                                     <button onClick={activateImagePaste}>Paste Image URL</button>
+//                                 </div>
+
+//                                 {imagePaste ? (
+//                                     <div>
+//                                         <form onSubmit={(e) => {
+//                                             e.preventDefault();
+//                                             edit(e);
+//                                             exitImagePaste();
+//                                         }}>
+//                                             Image URL: <input value={image} onChange={(e) => setImage(e.target.value)} />
+//                                             <input type="submit" value="Save" />
+//                                         </form>
+//                                     </div>
+//                                 ) : null}
+
+//                                 {imageSearch ? (
+//                                     <div>
+//                                         <form onSubmit={(e) => {
+//                                             e.preventDefault();
+//                                             fetchImages(image);
+//                                             exitImageSearch();
+//                                         }}>
+//                                             Search GIF: <input onInput={(e) => fetchImages(e.target.value)} />
+//                                             {previewImage && (
+//                                                 <div id="gif-preview">
+//                                                     <p>GIF Preview:</p>
+//                                                     <img src={previewImage} alt="Invalid Image URL" width="90" height="90" /><br />
+//                                                     <button type="button" onClick={fetchRandomImage}>Show Another GIF</button>
+//                                                 </div>
+//                                             )}
+//                                             <button type="submit">Go</button>
+//                                         </form>
+//                                     </div>
+//                                 ) : null}
+
+//                                 <form onSubmit={edit}>
+//                                     Word: <input value={name} onChange={(e) => setName(e.target.value)} /><br />
+//                                     <input type="submit" value="Save" />
+//                                 </form>
+
+//                                 <button onClick={exitEditMode}>Cancel Edit</button>
+//                             </div>
+//                         ) : (
+//                             <div>
+//                                 <button onClick={activateEditMode}>Edit</button>
+//                             </div>
+//                         )}
+
+//                         <div>
+//                             <button onClick={del}>Delete Card</button>
+//                         </div><br />
+//                     </div>
+//                 );
+//             })}
+
+//             <br />
+//             <Link to="/study">Back to Card Collection</Link>
+//         </div>
+//     );
+// }
+
+// export default Card;
+
+
--- client/src/components/StudyMode.js
+++ client/src/components/StudyMode.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL
+const GIPHY_KEY = process.env.REACT_APP_SEARCH_IMAGE_GIPHY_API_KEY
 
 const Study = (props) => {
     const [cards, setCards] = useState([]);
@@ -25,31 +26,9 @@ const Study = (props) => {
         }
     }
 
-    // const addCard = async (e) => {
-    //     e.preventDefault()
-
-    //     const options = {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ image, name }),
-    //     };
-
-    //     try {
-    //         const res = await fetch(`${BASE_URL}/api/cards`, options);
-    //         const data = await res.json();
-    //         setCards(data);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
-
-
-
     const fetchImages = async (searchWord) => {
         try {
-            const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`);
+            const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=${GIPHY_KEY}`);
             const giphyData = await res.json();
             if (giphyData && giphyData.data.length > 0) {
                 const randomIndex = Math.floor(Math.random() * giphyData.data.length);
@@ -75,7 +54,6 @@ const Study = (props) => {
         try {
             const res = await fetch(`${BASE_URL}/api/cards`, options);
             const data = await res.json();
-            // setCards(data);
             showAll()
         } catch (e) {
             console.log(e);
@@ -102,18 +80,6 @@ const Study = (props) => {
         setImageSearch(false);
         setImage('');
     };
-
-    // const fetchImages = async (searchWord) => {
-    //     try {
-    //         const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchWord}&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`);
-    //         const giphyData = await res.json();
-    //         if (giphyData && giphyData.data.length > 0) {
-    //             const randomIndex = Math.floor(Math.random() * giphyData.data.length);
-    //             const giphyImageNew = giphyData.data[randomIndex].images.original.url;
-    //             setImage(giphyImageNew);
-    //         }
-    //     } catch (e) { console.log(e) }
-    // }
 
     return (
         <div>
