effe435fc5f66650cc28b4c43386fd4256df7fc6
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-12-06 19:08:12 +0200

    d

--- client/src/components/QuizMode.js
+++ client/src/components/QuizMode.js
@@ -1,131 +1,123 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const Quiz = (props) => {
+const Quiz = () => {
     const [cards, setCards] = useState([]);
-    const [usedCards, setUsedCards] = useState([]);
-    const [randomCard, setRandomCard] = useState(null);
-    const [answer, setAnswer] = useState('');
+    const [shuffledCards, setShuffledCards] = useState([]);
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const [answer, setAnswer] = useState("");
     const [count, setCount] = useState(0);
     const [quizMode, setQuizMode] = useState(false);
 
     useEffect(() => {
-        if (quizMode) {
-            getCards()
-        } else {
-            setRandomCard(null);
-        }
-    }, [quizMode]);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${BASE_URL}/api/cards`);
+                const data = await response.json();
+
+                if (data.length > 0) {
+                    setCards(data);
+                } else {
+                    console.error("No cards available");
+                }
+            } catch (error) {
+                console.error("Error fetching cards:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     useEffect(() => {
         if (quizMode && cards.length > 0) {
-            getRandomCard();
+            const shuffled = [...cards].sort(() => Math.random() - 0.5);
+            setShuffledCards(shuffled);
+            setCurrentCardIndex(0);
         }
     }, [quizMode, cards]);
 
-    const getCards = async () => {
-        try {
-            const res = await fetch(`${BASE_URL}/api/cards`);
-            const data = await res.json();
-
-            if (data.length > 0) {
-                setCards(data);
-                setUsedCards([]);;
-                if (quizMode) {
-                    getRandomCard();
-                }
-            } else {
-                setCards([]);
-                setUsedCards([]);
-                setRandomCard(null);
-            }
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
     const getRandomCard = () => {
-        const cardsAvailable = cards
-            .map((_, index) => index)
-            .filter((_, index) => !usedCards.includes(index))
-
-        if (cardsAvailable.length === 0) {
-            setUsedCards([]);
+        if (currentCardIndex < shuffledCards.length) {
+            const currentCard = shuffledCards[currentCardIndex];
+            setCurrentCardIndex((prevIndex) => prevIndex + 1);
+            return currentCard;
         }
-
-        const randomIndex = Math.floor(Math.random() * cardsAvailable.length);
-        const randomCardIndex = cardsAvailable[randomIndex];
-
-        setRandomCard(cards[randomCardIndex]);
-        setUsedCards([...usedCards, randomCardIndex]);
-    }
+        return null;
+    };
 
     const startQuiz = () => {
         setQuizMode(true);
-        setUsedCards([]);
         setCount(0);
         setAnswer("");
-        if (cards.length > 0) {
-            getRandomCard();
-        }
-        // console.log('quiz mode =>', quizMode)
-    }
+        setCurrentCardIndex(0);
+    };
 
     const resetQuiz = () => {
         setQuizMode(false);
-        setUsedCards([]);
         setCount(0);
         setAnswer("");
-    }
+        setCurrentCardIndex(0);
+    };
 
     const handleAnswer = () => {
-        if (answer.toLowerCase() === randomCard.name.toLowerCase()) {
-            setCount(count + 1);
+        const currentCard = shuffledCards[currentCardIndex];
+
+        if (currentCard && answer.toLowerCase() === currentCard.name.toLowerCase()) {
+            setCount((prevCount) => prevCount + 1);
         }
 
-        if (usedCards.length === cards.length) {
-            setAnswer("");
-        } else {
-            getRandomCard();
-            setAnswer("");
+        const nextCard = getRandomCard();
+        if (!nextCard) {
+            setQuizMode(false);
         }
-    }
 
-    useEffect(() => {
-        console.log("quiz mode =>", quizMode);
-    }, [quizMode]);
+        setAnswer("");
+    };
 
     return (
         <div>
             <h1>Let's check your knowledge!</h1>
             {quizMode ? (
                 <div>
-                    {randomCard && cards.length !== 0 ? (
+                    {currentCardIndex < shuffledCards.length ? (
                         <>
-                            <img src={randomCard.image} alt="Ivalid Image URL" width="90" height="90" /><br />
-                            <input type="text" placeholder="Type the word from picture" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+                            <img
+                                src={shuffledCards[currentCardIndex].image}
+                                alt="Invalid Image URL"
+                                width="90"
+                                height="90"
+                            />
+                            <br />
+                            <input
+                                type="text"
+                                placeholder="Type the word from the picture"
+                                value={answer}
+                                onChange={(e) => setAnswer(e.target.value)}
+                            />
                             <button onClick={handleAnswer}>Next</button>
                             <p>Score: {count}/{cards.length}</p>
                         </>
                     ) : (
                         <>
-                            <>
-                                <p>This is your final score: {count}/{cards.length}</p>
-                                <button onClick={resetQuiz}>Click to practice again</button>
-                            </>
+                            <p>This is your final score: {count}/{cards.length}</p>
+                            <button onClick={resetQuiz}>Click to practice again</button>
                         </>
                     )}
                 </div>
             ) : (
                 <>
-                    <button onClick={startQuiz}>Start</button><br />
+                    <button onClick={startQuiz}>Start</button>
+                    <br />
                 </>
             )}
             <Link to="/">Back to Homepage</Link>
         </div>
     );
-}
+};
 
 export default Quiz;
+
+
+
