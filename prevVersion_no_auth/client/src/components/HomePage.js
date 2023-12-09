3b9e8bb2099d4f77c3eda470c8af69c9666a3f30
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-12-09 15:00:25 +0200

    design homepage ver no auth

--- prevVersion_no_auth/client/src/components/HomePage.js
+++ prevVersion_no_auth/client/src/components/HomePage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
 const HomePage = (props) => {
-    
+
     useEffect(() => {
     }, []);
 
@@ -10,8 +10,17 @@ const HomePage = (props) => {
         <div>
             <h1>Welcome to the Anki App</h1>
             <h4>Add some cards and practice your knowledge!</h4>
-            <Link to="/quiz">Quiz Mode</Link><br/>
-            <Link to="/study">Study Mode</Link>
+            <div className="linksHomePage">
+                <div className="linkIcon">
+                    <img src="../study.png" alt="Invalid Image URL" className="icon" />
+                    <Link to="/study" className="startLinks">Study Mode</Link><br /><br />
+                </div>
+                <div className="linkIcon">
+                    <img src="../quiz.png" alt="Invalid Image URL" className="icon" />
+                    <Link to="/quiz" className="startLinks">Quiz Mode</Link>
+                </div>
+            </div>
+            <img src="../image2.jpg" alt="Invalid Image URL" id="startImage" />
         </div>
 
     )
